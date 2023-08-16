5208c438e84a797b6f5d4ecc6e03d9807ef6ced1
BDadario <BDadario@gmail.com>
2023-08-16 18:36:38 -0400

    Update streamlit_app.py
    d

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,7 +2,8 @@ import streamlit
 import pandas
 import requests
 
-
+my_fruit_list = pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
+my_fruit_list = my_fruit_list.set_index('Fruit')
 
 # Display the table on the page.
 streamlit.dataframe(fruits_to_show)
