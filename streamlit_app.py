0ff5c06faa6e73d90c5463490ce3e78e3ce0393e
BDadario <BDadario@gmail.com>
2023-08-19 08:58:52 -0400

    Update streamlit_app.py
    Added code for user to select their own fruit.

--- streamlit_app.py
+++ streamlit_app.py
@@ -42,3 +42,8 @@ my_cur.execute("select * from pc_rivery_db.public.fruit_load_list")
 my_data_rows = my_cur.fetchall()
 streamlit.header("The fruit load list contains:")
 streamlit.dataframe(my_data_rows)
+
+#New section to ask user what fruite they would like to add?
+streamlit.text('What fruit would you like to add?')
+add_my_fruit = streamlit.text_input('What fruit would you like information about?')
+streamlit.text('Thanks for adding', add_my_fruit)
