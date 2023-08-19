477f7027c83e4096af0de3578e7dcd1e142a871e
BDadario <BDadario@gmail.com>
2023-08-19 09:24:24 -0400

    Update streamlit_app.py
    fixed adding fruit code section.

--- streamlit_app.py
+++ streamlit_app.py
@@ -44,8 +44,7 @@ streamlit.header("The fruit load list contains:")
 streamlit.dataframe(my_data_rows)
 
 #New section to ask user what fruite they would like to add?
-streamlit.text('What fruit would you like to add?')
-add_my_fruit = streamlit.text_input('What fruit would you like information about?')
+add_my_fruit = streamlit.text_input('What fruit would you like to add?')
 streamlit.write('Thanks for adding ', add_my_fruit)
 
 # This will not work correctly, but just go with it for now
